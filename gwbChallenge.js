b1510264abb8e0ea2412e1b8449f5a5a77f4c92b
EricMTodd <eterictodd@gmail.com>
2019-05-24 15:40:32 -0600

    Cleaned up process and completed challenge.

--- gwbChallenge.js
+++ gwbChallenge.js
@@ -1,18 +1,15 @@
 modifyString = str => {
   // Remove vowels
   // Reverse string
-  console.log(str);
-  testCase = "aeiouAEIOU";
   let newString = "";
 
-  for (let i = 0; i < str.length; i++) {
-    console.log(str[i]);
-    for (let k = 0; k < testCase.length; k++) {
-      if (str[i] === testCase[k]) {
-        console.log("Match");
-      }
-    }
-  }
+  newString = str.replace(/[aeiou]/gi, "");
+  newString = newString
+    .split("")
+    .reverse()
+    .join("");
+  console.log(newString);
+  return;
 };
 
 modifyString("Hello World");
